c815843561e14e23d2e9a0bf64a74243a71afee8
Mr.T-T <toan.vo2@tiki.vn>
2019-12-25 09:55:18 +0700

    update folder structure

--- features_sql/feature_screen_view.sql
+++ features_sql/feature_screen_view.sql
@@ -1,99 +1,60 @@
-with screen_class as 
-(select 
-event_date,
-event_timestamp,
-user_id,
-user_pseudo_id,
-category,
-mobile_model_name,
-mobile_brand_name,
-platform,
-version,
-region,
-event_name,
-screen_class_current,
-LAG(event_timestamp,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) AS event_timestamp_before,
-LAG(screen_class_current,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) AS screen_class_before,
-LEAD(event_timestamp,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) AS event_timestamp_after,
-LEAD(screen_class_current,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) AS screen_class_after,
-(event_timestamp - LAG(event_timestamp,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp))/(1000*1000*60) time_before,
-case when (LEAD(event_timestamp,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) - event_timestamp) > (5*1000*1000*60) then 0 else (LEAD(event_timestamp,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) - event_timestamp)/(1000*1000*60) end time_after,
-CASE 
-  WHEN screen_class_current <> LAG(screen_class_current,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) 
-  OR LAG(screen_class_current,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp)  IS NULL THEN 1 ELSE 0 
-  END is_new_screen,
-CASE
-  WHEN event_timestamp - LAG(event_timestamp,1,0) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) > (5*60*1000*1000)
-  OR LAG(event_timestamp,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) IS NULL THEN 1 ELSE 0
-  END AS is_new_session
-from 
-( 
-select 
-event_date,
-event_timestamp,
-user_id,
-user_pseudo_id,
-device.category,
-device.mobile_model_name,
-device.mobile_brand_name,
-platform,
-app_info.version,
-geo.region,
-event_name,
-params.value.string_value screen_class_current
-from
-`tikiandroid-1047.data_log.events_new`,
-unnest(event_params) as params
-where 
-1=1
-and event_date >= '2019-12-22'
-and event_date < '2019-12-23'
-and params.key = 'firebase_screen_class'
-)
-),
+WITH screen_class AS (
+     SELECT event_date, event_timestamp, user_pseudo_id, platform, screen_class_current
+            , LEAD(event_timestamp,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) AS event_timestamp_after
+            , LEAD(screen_class_current,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) AS screen_class_after
+            , CASE WHEN (LEAD(event_timestamp,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) - event_timestamp) > (60*1000*1000*60) 
+                            THEN 0 ELSE (LEAD(event_timestamp,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) - event_timestamp)/(1000*1000*60) 
+                            END AS time_after_minute
+            , CASE WHEN screen_class_current <> LAG(screen_class_current,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) 
+                    OR LAG(screen_class_current,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) IS NULL THEN 1 
+                    ELSE 0 
+                    END AS is_new_screen
+            , CASE WHEN event_timestamp - LAG(event_timestamp,1,0) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) > (60*60*1000*1000) 
+                    OR LAG(event_timestamp,1) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) IS NULL THEN 1 
+                    ELSE 0 
+                    END AS is_new_session 
+    FROM ( SELECT event_date, event_timestamp, user_pseudo_id, platform, params.value.string_value screen_class_current 
+            FROM `tikiandroid-1047.analytics_153801291.events_*`, UNNEST(event_params) AS params 
+            WHERE 1=1 
+            AND _TABLE_SUFFIX >= '20191001' --FORMAT_DATE('%Y%m%d', DATE_SUB(@run_date, INTERVAL 1 DAY)) 
+            AND _TABLE_SUFFIX < '20191101' AND params.key = 'firebase_screen_class' 
+            AND platform = 'ANDROID' AND params.value.string_value NOT IN ('SplashActivity')
 
-screen_class_1 as 
-(
-select 
-screen_class.*,
-SUM(is_new_screen) over (partition by user_pseudo_id order by event_timestamp) AS screen_id,
-SUM(is_new_session) over (partition by user_pseudo_id order by event_timestamp) AS session_id
-from screen_class
-)
+            UNION ALL 
 
-select
-screen_class_3.*,
-coalesce(svs.screen_name, screen_class_after) screen_name_after,
-coalesce(svs.section_l2, screen_class_after) section_l2_after,
-case when screen_class_after is null then 'Exit' else 'Not_exit' end Exit_screen
-from
-(
-select 
-screen_class_2.*,
-LEAD(screen_class_current,1) OVER (PARTITION BY user_pseudo_id ORDER BY screen_id) AS screen_class_after
-from
-(
-select 
-UNIX_MILLIS(timestamp(event_date)) created,
-event_date,
-user_pseudo_id,
-category,
-mobile_model_name,
-mobile_brand_name,
-sc1.platform,
-version,
-region,
-screen_class_current,
-coalesce(svs.screen_name, screen_class_current) screen_name_current,
-coalesce(svs.section_l2, screen_class_current) section_l2_current,
-session_id,
-screen_id,
-sum(time_after) time_spent,
-count(1) number_of_event
-from screen_class_1 sc1
-left join `tiki-dwh.consumer_product.screen_view_section_import` svs on sc1.screen_class_current = svs.firebase_screen_class and sc1.platform = svs.platform
-where 1=1
-group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14
-) screen_class_2 
-) screen_class_3 
-left join `tiki-dwh.consumer_product.screen_view_section_import` svs on screen_class_3.screen_class_after = svs.firebase_screen_class and screen_class_3.platform = svs.platform
+            SELECT event_date, event_timestamp, user_pseudo_id, platform, params.value.string_value screen_class_current 
+            FROM `tikiandroid-1047.analytics_153801291.events_*`, UNNEST(event_params) AS params 
+            WHERE 1=1 AND _TABLE_SUFFIX >= '20191001' --FORMAT_DATE('%Y%m%d', DATE_SUB(@run_date, INTERVAL 1 DAY)) 
+            AND _TABLE_SUFFIX < '20191101' --'20191101' 
+            AND params.key = 'page_view' --page_view 
+            AND platform = 'IOS' 
+            AND params.value.string_value NOT IN ('SplashActivity') ) )
+    
+    , screen_class_1 AS ( 
+        SELECT screen_class.*, SUM(is_new_screen) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) AS screen_id
+                            , SUM(is_new_session) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp) AS session_id 
+        FROM screen_class )
+    
+    , web_view AS ( SELECT date_key ,pagePath ,type ,pagePath_remove_param ,fullVisitorId ,visitId 
+                    FROM `tiki-dwh.dwh.web_page_info` 
+                    WHERE 1=1 
+                    AND date_key >= date(2019,10,01) 
+                    AND date_key < date(2019,11,01) ) 
+
+SELECT PARSE_DATE('%Y%m%d', event_date) event_date, user_pseudo_id deviceID
+        , COUNT(DISTINCT screen_class_current) no_of_screen
+        , COUNT(DISTINCT session_id) no_of_session
+        , STRING_AGG(DISTINCT screen_class_current) screen_class 
+FROM screen_class_1 sc1 
+WHERE 1=1 
+GROUP BY 1,2 
+
+UNION ALL 
+
+SELECT date_key, fullVisitorId
+        , COUNT(DISTINCT pagePath)
+        , COUNT(DISTINCT visitId)
+        , STRING_AGG(DISTINCT pagePath_remove_param) 
+FROM web_view 
+WHERE 1=1 
+GROUP BY 1,2	
